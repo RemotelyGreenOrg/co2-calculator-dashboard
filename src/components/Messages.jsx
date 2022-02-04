7e18a0ee76f51bcf4b03a2459af8910baffe2a22
Nicolas Zanotti <nicolaszanotti@me.com>
2022-02-04 14:23:29 +0100

    fix: connect to local websockets endpoint

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,84 +1,81 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Messages = () => {
-  
-  const [messages, setMessages] = useState([])
-  const [oneMessage, setOneMessage] = useState('')
-  
-  const [showButton, setShowButton] = useState(true)
-  const [formDisabled, setFormDisabled] = useState(false)
+  const [messages, setMessages] = useState([]);
+  const [oneMessage, setOneMessage] = useState("");
 
-  const [webSocket, setWebSocket] = useState()
+  const [showButton, setShowButton] = useState(false);
+  const [formDisabled, setFormDisabled] = useState(false);
+
+  const [webSocket, setWebSocket] = useState();
 
   //--------- form functions ---------------//
 
-  const onDisconnect = (event) => {
-    // event.preventDefault()
-    webSocket.close()
-  }
+  const onDisconnect = event => {
+    webSocket.close();
+  };
 
-  const onFormInputChange = (event) => {
-    const textInput = event.target.value
-    setOneMessage(textInput)
-    console.log(textInput)
-  }
+  const onFormInputChange = event => {
+    const textInput = event.target.value;
+    setOneMessage(textInput);
+  };
 
-  const onFormSubmit = (event) => {
-    event.preventDefault()
-    console.log(oneMessage)
-    console.log(webSocket)
+  const onFormSubmit = event => {
+    event.preventDefault();
     if (oneMessage !== "") webSocket.send(JSON.stringify({ message: oneMessage }));
-    setOneMessage('');
-  }
+    setOneMessage("");
+  };
 
   //-------------------------------------------//
   //--------- webSocket functions ---------------//
 
   function onWebSocketMessage(event) {
-    console.log('on websocket message')
     const data = JSON.parse(event.data);
-    
+
     if (data.hasOwnProperty("message")) {
       const time = new Date().toLocaleTimeString();
-      const message = `${time} \t ${data.message}`
-      setMessages([...messages, message])
+      const message = `${time} \t ${data.message}`;
+      setMessages(current => [...current, message]);
     }
   }
 
   function onWebSocketOpen(event) {
-    event.preventDefault()
-    setShowButton(false)
+    console.log("onWebSocketOpen");
+    setShowButton(true);
   }
 
   function onWebSocketClose(event) {
-    event.preventDefault()
-    setShowButton(true)
-    setFormDisabled(true)
+    setShowButton(false);
+    setFormDisabled(true);
   }
 
   useEffect(() => {
-  console.log('new websocket')
-  const webSocketUrl = (window.location.protocol === "https:" ? "wss:" : "ws:") + "//" + window.location.host;
-  const socket = new WebSocket(webSocketUrl)
-  socket.addEventListener("message", onWebSocketMessage);
-  socket.addEventListener("close", onWebSocketClose);
-  socket.addEventListener("open", onWebSocketOpen);
-    setWebSocket(socket)
-  }, [])
+    let webSocketUrl;
+    if (window.location.hostname === "localhost") webSocketUrl = "ws://localhost:8000";
+    else webSocketUrl = (window.location.protocol === "https:" ? "wss:" : "ws:") + "//" + window.location.host;
+
+    const socket = new WebSocket(webSocketUrl);
+    socket.addEventListener("message", onWebSocketMessage);
+    socket.addEventListener("close", onWebSocketClose);
+    socket.addEventListener("open", onWebSocketOpen);
+    setWebSocket(socket);
+  }, []);
 
   return (
     <div>
       <h1>Messages</h1>
       <ul id="messages">
-        {messages.map((m) => (
-          <li>{m}</li>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
         ))}
       </ul>
       <form onSubmit={onFormSubmit}>
-        <input type="text" id="message" name="message" onChange={onFormInputChange} autoComplete="off" required disabled={formDisabled} value={oneMessage}/>
+        <input type="text" id="message" name="message" onChange={onFormInputChange} autoComplete="off" required disabled={formDisabled} value={oneMessage} />
         <button disabled={formDisabled}>Send</button>
       </form>
-      <button id="disconnect" onClick={onDisconnect} style={{ visibility: showButton ? "visible" : "hidden" }}>Disconnect</button>
+      <button id="disconnect" onClick={onDisconnect} style={{ visibility: showButton ? "visible" : "hidden" }}>
+        Disconnect
+      </button>
     </div>
   );
 };
