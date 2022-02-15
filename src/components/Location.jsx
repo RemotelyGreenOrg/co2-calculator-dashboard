07e0c60fe6ca8cf3daf32b2e65ecee0053ac493f
eddie ruxton <ae.ruxton@gmail.com>
2022-02-15 11:48:05 +0000

    added geoapify autocomplete component - whittled it down to as few lines as possible. use later in create event form

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,25 +2,14 @@ import React, { useState } from 'react'
 import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
 import '@geoapify/geocoder-autocomplete/styles/minimal.css'
 
-const Location = () => {
-
-  const [position, setPosition] = useState();
-  const [value, setValue] = useState('');
-
-  // move this function into createEventForm & pass it as props to this component
-  // use it to set position (lat lon) then you can use post request to create event. 
-  function onPlaceSelect(value) {
-    console.log(value);
-      // setPosition -> lat lon 
-      // setValue -> address (textInput)
-  }
+const Location = ({
+  placeSelect
+}) => {
 
   return <div>
-    <GeoapifyContext apiKey="00a9862ac01f454887fc285e220d8460">
+    <GeoapifyContext apiKey="333a59e70c194914af823f45328e4a49">
       <GeoapifyGeocoderAutocomplete placeholder="Enter address here"
-        value={value}
-        position={position}
-        placeSelect={onPlaceSelect}
+        placeSelect={placeSelect}
         skipIcons={true}
         skipDetails={false}
       />
