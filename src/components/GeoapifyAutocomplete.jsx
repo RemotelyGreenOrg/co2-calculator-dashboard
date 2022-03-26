3e88ba93a507998c6d36f78327f4f3a6252f7182
Ben Krikler <4083697+benkrikler@users.noreply.github.com>
2022-03-26 19:40:35 -0300

    Add country flags before option

--- src/components/GeoapifyAutocomplete.jsx
+++ src/components/GeoapifyAutocomplete.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import {useState, useMemo, useEffect} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
@@ -7,47 +7,19 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import parse from 'autosuggest-highlight/parse';
 import throttle from 'lodash.throttle';
+import {flag} from "country-emoji";
 
 // This key was created specifically for the demo in mui.com.
 // You need to create a new one for your application.
 const GEOAPIFY_API_KEY = "333a59e70c194914af823f45328e4a49";
 
 
-function addFeatureIcon(element, type, countryCode) {
-  const iconMap = {
-      'unknown': 'map-marker',
-      'amenity': 'map-marker',
-      'building': 'map-marker',
-      'street': 'road',
-      'suburb': 'city',
-      'district': 'city',
-      'postcode': 'city',
-      'city': 'city',
-      'county': 'city',
-      'state': 'city'
-  };
-  const countryData = countries_json_1.default.find(county => countryCode && county.code.toLowerCase() === countryCode.toLowerCase());
-  if ((type === 'country') && countryData) {
-      element.classList.add("emoji");
-      ;
-      const emojiElement = document.createElement('span');
-      emojiElement.innerText = countryData.emoji;
-      element.appendChild(emojiElement);
-  }
-  else if (iconMap[type]) {
-      this.addIcon(element, iconMap[type]);
-  }
-  else {
-      this.addIcon(element, 'map-marker');
-  }
-}
-
 export default function GeoapifyAutocomplete({label, required}) {
-  const [value, setValue] = React.useState(null);
-  const [inputValue, setInputValue] = React.useState('');
-  const [options, setOptions] = React.useState([]);
+  const [value, setValue] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState([]);
 
-  const query = React.useMemo(
+  const query = useMemo(
    () =>
      throttle((request, callback) => {
         const promise = new Promise((resolve, reject) => {
@@ -75,7 +47,7 @@ export default function GeoapifyAutocomplete({label, required}) {
     [],
   );
 
-  React.useEffect(() => {
+  useEffect(() => {
     let active = true;
 
     if (inputValue === '') {
@@ -107,7 +79,6 @@ export default function GeoapifyAutocomplete({label, required}) {
   return (
     <Autocomplete
       id="geoapify-autocomplete"
-      //sx={{ width: 300 }}
       getOptionLabel={(option) =>
         typeof option === 'string' ? option : option.formatted
       }
@@ -134,10 +105,12 @@ export default function GeoapifyAutocomplete({label, required}) {
           <li {...props}>
             <Grid container alignItems="center">
               <Grid item>
-                <Box
-                  component={LocationOnIcon}
-                  sx={{ color: 'text.secondary', mr: 2 }}
-                />
+                {option.country_code
+                  ? <Typography variant="body1" pr={2}>{flag(option.country_code)}</Typography>
+                  : <Box component={LocationOnIcon}
+                      sx={{ color: 'text.secondary', mr: 2 }}
+                      />
+                }
               </Grid>
               <Grid item xs>
                 <Typography variant="body1" color="text.secondary">
