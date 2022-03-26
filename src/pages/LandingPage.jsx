884c2b23541775b4da0f8d67810eb3c344fa59db
Ben Krikler <4083697+benkrikler@users.noreply.github.com>
2022-03-26 19:15:05 -0300

    Add material container to landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,13 +2,24 @@ import { NavLink } from "react-router-dom";
 import JoinEventForm from "../components/JoinEventForm";
 import CreateEventForm from "../components/CreateEventForm";
 
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
 function LandingPage(){
   return (
     <>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Typography component="h1" variant="h4" align="center">
+            Realtime event calculator
+          </Typography>
     <CreateEventForm/>
     <br/>
     <h3> Or join an existing event </h3>
     <JoinEventForm/>
+        </Paper>
+      </Container>
     </>
   );
 }
