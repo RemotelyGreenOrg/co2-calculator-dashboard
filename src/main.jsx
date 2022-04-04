bc8f29dcee8a827060bc16a5451438b5ea6592c7
Ben Krikler <4083697+benkrikler@users.noreply.github.com>
2022-04-04 12:14:56 +0200

    Add Google Analytics to landing page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-B9WH7S75Z2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
