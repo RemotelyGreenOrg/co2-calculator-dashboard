95e379c75fb8c61d3b777ef659cf18c43db8854e
benkrikler <4083697+benkrikler@users.noreply.github.com>
2025-03-04 11:25:22 +0100

    Add Analytics to landing page (#10)
    * Add Google Analytics to landing page
    
    * Add loading skeleton animation
    
    * feat: respect the "do not track" browser setting
    
    ---------
    
    Co-authored-by: Nicolas Zanotti <nicolaszanotti@me.com>

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
+import ReactGA from 'react-ga';
+
+if (navigator.doNotTrack !== "1") {
+  ReactGA.initialize('G-B9WH7S75Z2');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(
   <React.StrictMode>
